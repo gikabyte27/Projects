9e9f686f67ab607f2bf4a755acd1d94095c6edc1
George Tudor <tudor.george002@gmail.com>
2023-07-22 14:59:59 +0300

    Add password rules. Improved scripts

--- setup_tools/install.sh
+++ setup_tools/install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+sudo apt-get update && sudo apt-get -y install slapd ldap-utils && sudo systemctl enable slapd
 apt install -y golang
 ZSH_CONFIG=$HOME/.zshrc
 BASH_CONFIG=$HOME/.bashrc
 PROFILE=$HOME/.profile
+
+
+
 cat go.config >> $ZSH_CONFIG
 cat go.config >> $BASH_CONFIG 
 cat go.config >> $PROFILE
@@ -15,3 +19,8 @@ go install -v github.com/tomnomnom/assetfinder@latest
 
 git clone https://github.com/devanshbatham/ParamSpider
 pip3 install -r ParamSpider/requirements.txt
+
+pip install git+https://github.com/blacklanternsecurity/trevorproxy
+pip install git+https://github.com/blacklanternsecurity/trevorspray
+
+
