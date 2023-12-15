a4875e34c158b813803e565e9bddcc9c38d63baa
George Tudor <tudor.george002@gmail.com>
2023-12-15 23:15:20 +0200

    New Additions to setup scripts

--- setup_tools/install.sh
+++ setup_tools/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update -y --force-yes && sudo apt-get -y --force-yes install netcat-openbsd net-tools bmon tcpdump tmux python3-full python3-pip
+sudo apt-get update -y --force-yes && sudo apt-get -y --force-yes install netcat-openbsd net-tools bmon tcpdump tmux python3-full python3-pip nmap ffuf
 apt install -y golang
 ZSH_CONFIG=$HOME/.zshrc
 BASH_CONFIG=$HOME/.bashrc
@@ -18,9 +18,10 @@ source $PROFILE
 go install -v github.com/haccer/subjack@latest
 go install -v github.com/tomnomnom/waybackurls@latest
 go install -v github.com/tomnomnom/assetfinder@latest
-go install -v github.com/ffuf/ffuf/v2@latest
+go install -v github.com/owasp-amass/amass/v4/...@master
 
 git clone https://github.com/devanshbatham/ParamSpider /opt/ParamSpider
+git clone https://github.com/aboul3la/Sublist3r.git /opt/Sublist3r && pip install -r /opt/Sublist3r/requirements.txt --break-system-packages && pip3 install /opt/Sublist3r/ --break-system-packages
 wget -c https://github.com/danielmiessler/SecLists/archive/master.zip -O /usr/share/SecList.zip \
 	  && unzip /usr/share/SecList.zip -d /usr/share/ \
 		  && rm -f /usr/share/SecList.zip \
